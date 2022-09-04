3b8fb7f9f60871e363bad7909073c8d2f3dcd3ae
KyngCoder <keshawnrkmerchant9@gmail.com>
2022-09-04 11:23:32 -0500

    sidebar

--- pages/profile/Profile.tsx
+++ pages/profile/Profile.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Sidebar from '../../components/profile/Sidebar'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <section className="md:flex block w-screen">
+      <div className="bg-blue-500 md:w-1/5 w-1/3  h-screen">
+<Sidebar />
+      </div>
+      <div className="bg-green-300 w-4/5 h-full">
+main
+      </div>
+    </section>
   )
 }
 
